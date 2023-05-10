9c3c40d5a578a81d3ec8a9d4e40bd8ee788687ba
ashraful2020 <mr.ashraful2020@gmail.com>
2023-05-10 08:29:43 +0600

    perf: :zap: Improve performances
    make this comment to understand conventional commit
    
    1 2 3 4 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
+import type { Metadata } from "next";
 import Link from "next/link";
 
+export const metadata: Metadata = {
+  twitter: {
+    card: "app",
+    title: "Next.js",
+    description: "The React Framework for the Web",
+    siteId: "1467726470533754880",
+    creator: "@nextjs",
+    creatorId: "1467726470533754880",
+    images: {
+      url: "https://nextjs.org/og.png",
+      alt: "Next.js Logo",
+    },
+    app: {
+      name: "twitter_app",
+      id: {
+        iphone: "twitter_app://iphone",
+        ipad: "twitter_app://ipad",
+        googleplay: "twitter_app://googleplay",
+      },
+      url: {
+        iphone: "https://iphone_url",
+        ipad: "https://ipad_url",
+      },
+    },
+  },
+};
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] mt-28 font-bold text-6xl">
         Next Typescript
       </div>
- 
+
       <h1>
         <Link href="/about">About Page</Link>
       </h1>
